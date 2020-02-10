570f3c0ca420e62058d6305bdd8fe7824e852931
Adam Isaac <aci747@engineer.com>
2020-02-09 22:46:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Services Scoring Engine
+A CCDC style scoring engine for services, written in PHP.
+
+Putting this anywhere web facing would be a bad idea. 
---
+++ admin.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>[ADMIN] Services Scoring Engine</title>
+
+    <link rel="stylesheet" href="staticstyles.css">
+
+</head>
+
+<body>
+    <header>
+        <h1>CSG Services Scoring Engine - Admin Panel</h1>
+    </header>
+    <div class="sidenav">
+    <a href="index.php">Back to scoring engine</a>
+    <a href="">Github</a>
+    </div>
+
+
+</body>
+
+</html>
